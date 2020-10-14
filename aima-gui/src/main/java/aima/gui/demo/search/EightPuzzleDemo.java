9627a5ce7f22b6e460fd4693a4b552d21e9029d8
Aitor <aitorastur99@gmail.com>
2020-10-14 10:35:49 +0200

    Banco de instancias 8 puzzle

--- aima-gui/src/main/java/aima/gui/demo/search/EightPuzzleDemo.java
+++ aima-gui/src/main/java/aima/gui/demo/search/EightPuzzleDemo.java
@@ -30,20 +30,76 @@ public class EightPuzzleDemo {
 //	private static EightPuzzleBoard extreme =
 //			new EightPuzzleBoard(new int[] { 0, 8, 7, 6, 5, 4, 3, 2, 1 });
 
-	private static EightPuzzleBoard initial = 
-		//	new EightPuzzleBoard(new int[] { 1, 4, 2, 7, 5, 8, 3, 0, 6 });
-	 //new EightPuzzleBoard(new int[] { 1, 2, 5, 3, 4, 0, 6, 7, 8 });
-	 //new EightPuzzleBoard(new int[] { 0, 8, 7, 6, 5, 4, 3, 2, 1 });
+	private static EightPuzzleBoard initial = new EightPuzzleBoard(new int[]
+			 // { 1, 4, 2, 7, 5, 8, 3, 0, 6 });
+			// new EightPuzzleBoard(new int[] { 1, 2, 5, 3, 4, 0, 6, 7, 8 });
+			// new EightPuzzleBoard(new int[] { 0, 8, 7, 6, 5, 4, 3, 2, 1 });
 
-	// 20 Movimientos
-	// new EightPuzzleBoard(new int[] { 6,2,7,4,5,1,0,8,3 });
-	// new EightPuzzleBoard(new int[] {4,7,2,1,0,6,3,5,8});
-	//new EightPuzzleBoard(new int[] {7,1,5,4,0,8,2,6,3});
-	//new EightPuzzleBoard(new int[] {5,1,6,4,0,3,8,7,2});
-	 new EightPuzzleBoard(new int[] {2,4,0,6,3,1,7,8,5});
+			// Banco de ejemplos para el 8-puzzle. 
+			// Objetivo {1, 2, 3, 8, 0, 4, 7, 6, 5}
+			// Situaciones iniciales:
+			// 5 movimientos
+				{1, 0, 3, 8, 2, 5, 7, 4, 6}
+//				{1, 4, 2, 0, 8, 3, 7, 6, 5} 
+//				{1, 3, 4, 8, 6, 2, 7, 0, 5}
+//				{1, 2, 3, 7, 8, 0, 6, 5, 4}
+//				{1, 4, 2, 8, 3, 0, 7, 6, 5}
+//				{1, 2, 3, 0, 8, 6, 7, 5, 4}
+//				{1, 2, 3, 0, 4, 5, 8, 7, 6}
+//				{1, 0, 2, 8, 6, 3, 7, 5, 4}
+//				{2, 8, 3, 1, 6, 4, 7, 0, 5}
+			// 10 movimientos	
+//				{8, 2, 1, 7, 0, 3, 6, 5, 4}
+//				{1, 4, 0, 8, 5, 2, 7, 3, 6}
+//				{8, 1, 3, 7, 0, 5, 4, 2, 6}
+//				{8, 1, 2, 4, 0, 6, 7, 5, 3}
+//				{0, 1, 3, 7, 2, 5, 4, 8, 6}
+//				{1, 4, 0, 7, 8, 2, 6, 5, 3}
+//				{3, 8, 4, 1, 6, 2, 0, 7, 5}
+//				{1, 3, 4, 6, 7, 2, 0, 8, 5}
+//				{3, 2, 4, 1, 0, 5, 8, 7, 6}
+//				{8, 1, 0, 2, 5, 3, 7, 4, 6}
+			// 15 movimientos
+//				{4, 8, 2, 6, 3, 5, 1, 0, 7}
+//				{1, 4, 5, 2, 7, 0, 8, 6, 3}
+//				{1, 3, 8, 6, 7, 4, 2, 0, 5}
+//				{2, 0, 8, 7, 5, 3, 4, 1, 6}
+//				{7, 1, 3, 4, 5, 0, 8, 2, 6}
+//				{1, 3, 6, 7, 2, 0, 4, 5, 8}
+//				{7, 0, 3, 5, 1, 8, 2, 6, 4}
+//				{6, 3, 5, 2, 1, 0, 8, 4, 7}
+//				{6, 0, 3, 8, 1, 5, 4, 2, 7}
+//				{7, 8, 3, 1, 5, 0, 4, 2, 6}
+			// 20 movimientos
+//				{6, 2, 7, 4, 5, 1, 0, 8, 3}
+//				{4, 7, 2, 1, 0, 6, 3, 5, 8}
+//				{7, 1, 5, 4, 0, 8, 2, 6, 3}
+//				{5, 1, 6, 4, 0, 3, 8, 7, 2}
+//				{7, 1, 4, 5, 0, 6, 3, 2, 8}
+//				{2, 4, 0, 6, 3, 1, 7, 8, 5}
+//				{3, 5, 6, 2, 4, 7, 0, 1, 8}
+//				{1, 4, 7, 6, 8, 5, 0, 3, 2}
+//				{6, 4, 0, 2, 8, 1, 7, 3, 5}
+//				{4, 1, 3, 7, 2, 8, 5, 6, 0}
+			// 25 movimientos
+//				{6, 7, 4, 0, 5, 1, 3, 2, 8}
+//				{6, 0, 7, 5, 4, 1, 3, 8, 2}
+//				{3, 4, 8, 5, 7, 1, 6, 0, 2}
+//				{4, 5, 3, 7, 6, 2, 8, 0, 1}
+//				{2, 7, 8, 5, 4, 0, 3, 1, 6}
+			// 30 movimientos
+//				{5, 6, 7, 2, 8, 4, 0, 3, 1}
+//				{5, 6, 7, 4, 0, 8, 3, 2, 1}
+//				{5, 4, 7, 6, 0, 3, 8, 2, 1} 
+//				{3, 8, 7, 4, 0, 6, 5, 2, 1}
+//				{5, 6, 3, 4, 0, 2, 7, 8, 1}
+			// Instancias especiales
+//				{ 6, 3, 2, 5, 7, 8, 0, 4, 1 } 	// 24 movimientos
+//				{ 6, 3, 2, 5, 8, 1, 4, 0, 7 } 	// 23 movimientos
+//				{ 3, 5, 6, 4, 2, 7, 0, 8, 1 } 	// 24 movimientos
+//				{ 7, 0, 2, 3, 6, 1, 5, 8, 4 } 	// 21 movimientos
+	);
 
-	// 30 Movimientos
-	// new EightPuzzleBoard(new int[] { 5,6,7,2,8,4,0,3,1 });
 	public static void main(String[] args) {
 		System.out.println("Initial State:\n" + initial);
 		System.out.println("Goal  State:\n" + EightPuzzleFunctions.GOAL_STATE);
@@ -56,14 +112,14 @@ public class EightPuzzleDemo {
 		// eightPuzzleSimulatedAnnealingDemo();
 
 		// Practica 2:
-		eightPuzzleAStarNullHeristicDemo(); //h0
-	//eightPuzzleAStarWeigthedMisplaceDemo();//h1
-	//eightPuzzleAStarWeightedManhattanDemo();//h2
-		//eightPuzzleAStarNonConsistentHeuristicDemo(); //h3
-		//eightPuzzleAStarWeigthedNonConsistentHeuristicDemo(); //h3-2
-		eightPuzzleAStarEpsilonWeightedManhattanDistanceDemo(); //h4
+		eightPuzzleAStarNullHeristicDemo(); // h0
+		// eightPuzzleAStarWeigthedMisplaceDemo();//h1
+		// eightPuzzleAStarWeightedManhattanDemo();//h2
+		// eightPuzzleAStarNonConsistentHeuristicDemo(); //h3
+		// eightPuzzleAStarWeigthedNonConsistentHeuristicDemo(); //h3-2
+		eightPuzzleAStarEpsilonWeightedManhattanDistanceDemo(); // h4
 	}
-	
+
 	private static void eightPuzzleAStarNullHeristicDemo() {
 		System.out.println("\nEightPuzzleDemo AStar Search(MissplacedTileHeuristic) (9)");
 		try {
@@ -77,7 +133,6 @@ public class EightPuzzleDemo {
 			e.printStackTrace();
 		}
 	}
-	
 
 	private static void eightPuzzleAStarWeigthedMisplaceDemo() {
 		System.out.println("\nEightPuzzleDemo AStar Search(MissplacedTileHeuristic) (9)");
@@ -106,8 +161,7 @@ public class EightPuzzleDemo {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	private static void eightPuzzleAStarNonConsistentHeuristicDemo() {
 		System.out.println("\nEightPuzzleDemo AStar Search (ManhattanHeursitic)");
 		try {
@@ -121,7 +175,7 @@ public class EightPuzzleDemo {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static void eightPuzzleAStarWeigthedNonConsistentHeuristicDemo() {
 		System.out.println("\nEightPuzzleDemo AStar Search (ManhattanHeursitic)");
 		try {
@@ -135,7 +189,7 @@ public class EightPuzzleDemo {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static void eightPuzzleAStarEpsilonWeightedManhattanDistanceDemo() {
 		System.out.println("\nEightPuzzleDemo AStar Search(MissplacedTileHeuristic) (9)");
 		try {
